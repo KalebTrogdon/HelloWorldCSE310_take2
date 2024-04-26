8a167834527984404ad984d54192d86053526f7b
Kaleb Trogdon <67351277+KalebTrogdon@users.noreply.github.com>
2024-04-26 12:23:38 -0600

    added math

---
+++ testfile.py
@@ -0,0 +1,11 @@
+import math
+
+
+def calculate_circle_area(radius):
+    area = math.pi * radius * radius
+    return area
+
+area = calculate_circle_area(10)
+print(f'the area of the circle is: {area}')
+    
+
